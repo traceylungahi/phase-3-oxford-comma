eca99201352feb1b0f29d4cbc6f3d7c172e85229
TraceyMmboneLungahi <t.lungahi@gmail.com>
2022-08-30 16:19:07 +0300

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    if array.count > 2 
+        array[-1] = "and #{array.last}"
+        array.join(", ")
+    elsif array.count > 1 
+        array.join(" and ")
+    else
+        array.join
+    end     
 end
